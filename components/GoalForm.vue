6cd5c922a9cbfdba2f30aff323c90590bb067adf
Tyler Dawson <tddawson@gmail.com>
2025-04-24 08:44:15 -0700

    Basic goal form

---
+++ components/GoalForm.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+// import { createClient } from "@supabase/supabase-js";
+
+// const config = useRuntimeConfig();
+
+const eventItems = ["5k", "10k", "Half Marathon", "Marathon"];
+const event = ref("5k");
+const goal = ref("");
+
+// const supabase = createClient(
+//   config.public.supabaseUrl,
+//   config.public.supabaseKey,
+//   {
+//     accessToken: () => {
+//       return Clerk.session?.getToken();
+//     },
+//   }
+// );
+
+const submit = () => {};
+</script>
+
+<template>
+  <div class="flex mb-4 items-end">
+    <UFormField label="Event" class="mr-2">
+      <USelect v-model="event" :items="eventItems" class="w-48" />
+    </UFormField>
+    <UFormField label="Goal Time" class="mr-2">
+      <UInput v-model="goal" placeholder="HH:MM:SS" />
+    </UFormField>
+    <UButton
+      @click="submit"
+      icon="i-lucide-plus"
+      color="primary"
+      variant="solid"
+    />
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,4 @@
 <script setup lang="ts">
-import { createClient } from "@supabase/supabase-js";
-
-const config = useRuntimeConfig();
-
-const supabase = createClient(
-  config.public.supabaseUrl,
-  config.public.supabaseKey,
-  {
-    accessToken: () => {
-      return Clerk.session?.getToken();
-    },
-  }
-);
-
-console.log(supabase);
-
-const test = () => {
-  console.log("test");
-};
 </script>
 <template>
   <SignedOut>
@@ -26,6 +7,5 @@ const test = () => {
   <SignedIn>
     <UserButton />
   </SignedIn>
-  <h1>Hello</h1>
-  <UButton @click="test">Button</UButton>
+  <GoalForm />
 </template>
